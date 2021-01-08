c95fac7b0c23ce7160c01a394fc04f8b8afdb131
Kegan Ronholt <klronholt@mail.fhsu.edu>
2021-01-08 08:37:32 -0600

    tweaked debug

--- CRM/settings.py
+++ CRM/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ['secret_key']
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', 'nimble-data.herokuapp.com']
 
